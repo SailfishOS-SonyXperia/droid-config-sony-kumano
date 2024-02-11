SailfishOS-SonyXperia/droid-config-sony-kumano

commit 055d0b863baa6e1ecee89657917712b6355ec6de

flashing: Mention Xperia 1/5 instead of Xperia 10 II
[flashing] Mention Xperia 1/5 instead of Xperia 10 II. Fixes SailfishOS-SonyXperia/bugs#24

Signed-off-by: Björn Bidar <bjorn.bidar@thaodan.de>

sparse/boot/flash-on-windows.bat
@@ -120,7 +120,7 @@ del %tmpflashfile% >NUL 2>NUL
 setlocal EnableDelayedExpansion
 
 :: Find the blob image. Make sure there's only one.
-for /r %%f in (*_v12b_seine.img) do (
+for /r %%f in (*_v12a_kumano.img) do (
 if not defined blobfilename (
 REM Take only the filename and strip out the path which otherwise is there.
 REM This is to make sure that we do not face issues later with e.g. spaces in the path etc.

commit 0175f683ff4f023a3dc27dd6eff0062a62806f79

flashing: Fix more Xperia 1/5 mentions
Some mentions of the Xperia 10 II remain, remove them.

Fixes: 055d0b863baa6e1ecee8 ("flashing: Mention Xperia 1/5 instead of Xperia 10 II")

sparse/boot/flash-on-windows.bat
@@ -1,6 +1,6 @@
 @echo off
 
-:: This is simple windows flashing script for Sony Xperia 10 device
+:: This is simple windows flashing script for Sony Xperia 1/5 device
 :: This script is using fastboot to flash which differs from the factory method.
 
 set tmpflashfile=tmpfile.txt
@@ -11,7 +11,7 @@ set fastbootkillretval=0
 set serialnumbers=
 
 echo(
-echo This is a Windows flashing script for Sony Xperia 10 device.
+echo This is a Windows flashing script for Sony Xperia 1/5 device.
 echo(
 echo Power on the device in fastboot mode, by doing the following:
 echo 1. Turn off your Xperia.
@@ -143,10 +143,10 @@ echo Please download it from
 echo %oemblobwebsite%
 echo(
 echo Ensure you download the supported version of the image found under:
-echo "Software binaries for AOSP Android 10.0 - Kernel 4.14 - Seine"
+echo "Software binaries for AOSP Android 10.0 - Kernel 4.14 - Kumano"
 echo and unzip it into this directory.
 echo Note: information on which versions are supported is written in our Sailfish X
-echo installation instructions online.
+echo installation instructions online https://jolla.com/sailfishxinstall
 echo(
 echo Press enter to open the browser with the webpage.
 echo(